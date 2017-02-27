f21c0c6a5063f92962237ec832bf30091785bbdb
F Grayson Meindl <meindlgrayson@gmail.com>
2017-02-27 10:50:27 -0800

    5 entertainment questions added. comments added.

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 'use strict';
-
+//Question Constructor Below This Line
 function Question(question, right, wrongOne, wrongTwo, wrongThree, category) {
   this.question = question;
   this.right = right,
@@ -8,6 +8,8 @@ function Question(question, right, wrongOne, wrongTwo, wrongThree, category) {
   this.wrongThree = wrongThree;
   this.category = category;
 }
+
+//Sports Questions Below This Line
 
 var sportOne = new Question('When did Kansas University last win the NCAA National Championship?', '2008', '2012', '1994', '1956', 'sports');
 
@@ -18,3 +20,15 @@ var sportThree = new Question('What record number of beers did MLB Hall of Famer
 var sportFour = new Question('When was the first celebratory champagne bottle pop after a motor race?', 'The 1967 24 Hours of Le Mans', 'The 1958 Monaco Grand Prix', 'The 1987 Sonoma Motor Classic Festival', 'The 1974 Monterey Motor Classic', 'sports');
 
 var sportFive = new Question('Who is the last non-quarterback to win NFL MVP?', 'Adrian Peterson', 'Ray Lewis', 'LaDainian Tomlinson', 'Shaun Alexander', 'sports');
+
+//Entertainment Questions Below This Line
+
+var entertainOne = new Question('When asked what he had done with his life to merit an invitation to the Whitehouse, to which First Lady did Miles Davis respond: “Well, I’ve changed the course of music five or six times. What have you done except f**k the president?”', 'Nancy Reagan', 'Barbara Bush', 'Jackie Kennedy', 'Betty Ford', 'entertainment');
+
+var entertainTwo = new Question('In the film Pineapple Express, which Star Wars creature is referenced by James Franco during his captivity?', 'Rancor', 'Tauntaun', 'Jawa', 'Wookie', 'entertainment');
+
+var entertainThree = new Question('Who wrote the poetry spoken in Beyonce\'s Lemonade film', 'Warsan Shire', 'Roxane Gay', 'Chiminanda Ngozi Adichi', 'bell hooks', 'entertainment');
+
+var entertainFour = new Question('In what year was Wu-Tang member Raekwon’s gold-selling “Only Built for Cuban Linx” released?', '1995', '1994', '1996', '1997', 'entertainment');
+
+var entertainFive = new Question('Which Hip-Hop artist has acted in films alongside Edward Norton, Jack Black, and Bruce Willis?', 'Mos Def', 'Ludacris', 'Queen Latifah', 'Ice Cube', 'entertainment');
