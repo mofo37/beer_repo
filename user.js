6776bf7df6690e5cc185a33468c021ead242d52a
Keeley Hammond <keeleymhammond@gmail.com>
2017-02-28 11:38:00 -0800

    Starting to store user data

--- user.js
+++ user.js
@@ -1,26 +1,50 @@
 'use strict';
 
 // store all users
-allUsers = [];
+var allUsers = [];
 
 // create constructor for new user
-function User(name, drink, score) {
+function User(name) {
   this.name = name;
-  this.drink = drink;
-  this.score = score;
+  this.drink = 0;
+  this.score = 0;
 }
 
+// Gather User Data //
 // add event listener to the user submission form
-userForm = document.getElementById('user-form');
+var userForm = document.getElementById('user-form');
 userForm.addEventListener('submit', generateUser);
 
 // generate a new user from the event handler
 function generateUser(event) {
+  event.preventDefault();
   var userName = event.target.username.value;
-  var userEmail = event.target.email.value;
-  var userDrink = event.target.drink.value;
+  console.log(userName);
+  // var userDrink = event.target.drink.value;
 
-  var newUser = new User(userName, userEmail, userDrink);
+  var newUser = new User(userName);
   allUsers.push(newUser);
+
+  event.target.username.value = '';
+}
+
+// Store and Retrieve All User Data
+function storeData() {
+  var userDataJSON = JSON.stringify(allUsers);
+  localStorage.setItem('userData', userDataJSON);
+}
+
+function retrieveData(retrieved) {
+  allUsers = JSON.parse(retrieved);
+}
+
+// Start Game and Retrieve Data
+var startButton = document.getElementById('play');
+startButton.addEventListener('click', loadGame);
+
+function loadGame(event) {
+  event.preventDefault();
+  storeData();
+  window.location.replace('play.html');
 }
 
